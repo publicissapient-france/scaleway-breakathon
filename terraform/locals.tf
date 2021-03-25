1f37fe53bb8248d800a3dbd64224601045043470
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2021-03-25 13:08:00 +0100

    🐛 fix(tf/locals): fix tags syntax to be a list instead of dict

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,9 +1,9 @@
 locals {
   # Common tags to be assigned to all resources
-  common_tags = {
-    created-by      = terraform
-    breakathon-team = xebia4ever
-  }
+  common_tags = [
+    "created-by=terraform",
+    "breakathon-team=xebia4ever"
+  ]
 
   desc_purpose = "for the Scaleway Breakathon by the Xebia4Ever team"
 }
