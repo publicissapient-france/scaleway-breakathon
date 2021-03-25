894c5fc73433a785cd773622924b5d7881871546
franck cussac <franck.cussac@gmail.com>
2021-03-25 20:19:23 +0100

    fix app

--- ai/level1/client/src/Dropzone/Dropzone.js
+++ ai/level1/client/src/Dropzone/Dropzone.js
@@ -9,7 +9,7 @@ function Dropzone({onResult}) {
   const onDrop = useCallback(acceptedFiles => {
     const formData = new FormData();
     formData.append('file', acceptedFiles[0])
-    fetch('http://digit-finder-model/predict', {method: 'POST', body: formData})
+    fetch('/predict', {method: 'POST', body: formData})
     .then(response => response.json())
     .then(onResult);
   }, [onResult])
