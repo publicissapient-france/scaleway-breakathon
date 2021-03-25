2b121282e2b69b66f7e142cc3a74997181d39285
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2021-03-25 18:01:50 +0100

    🐛 fix(argocd): set admin password mtime to allow validation

--- terraform/kapsule-breakathon-cluster/k8s-addon-external-dns.tf
+++ terraform/kapsule-breakathon-cluster/k8s-addon-external-dns.tf
@@ -12,7 +12,7 @@ resource "helm_release" "external_dns_public_ingress" {
   namespace   = "k8s-addons"
 
   atomic      = true
-  depends_on  = [kubernetes_namespace.k8s-addons] # TODO cluster
+  depends_on  = [kubernetes_namespace.k8s-addons]
   values      = [
     templatefile("${path.module}/k8s-addon-external-dns-values.yaml", {
       txt_owner = "external-dns-xebia4ever-breakathon-${var.challenge_slug}",
